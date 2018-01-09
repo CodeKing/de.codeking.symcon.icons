bb044c54ae37657cd8b02d91a29013813bd00357
Frank <frank@codeking.de>
2018-01-09 12:51:01 +0100

    batch for creating css added

---
+++ build.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: variables
+set icons=
+
+:: clear current css file
+break>icons.css
+
+:: change to icons folder
+cd icons
+
+:: loop icons and appent to css file
+for /R %%f in (*.png) do (
+    set icons=!icons! "%%~nf",
+    @echo .ipsIcon%%~nf { color: transparent^; min-height: 32px^; min-width: 32px^; background-position: center center^; background-repeat: no-repeat^; background-image: url^(icons/%%~nf%%~xf^)^;} >> ../icons.css
+)
+
+:: remove last ; and whitespaces
+set icons=!icons:~1!
+set icons=!icons:~0,-1!
+
+:: echo output
+echo JSON Definitionen:
+echo ====================================================================
+echo(
+echo.%icons%
+echo(
+echo(
+
+pause
